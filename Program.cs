af8ab2376e380c777e0d9b4da98b94f44ba2b405
Bárbara <babyr.oliveira@gmail.com>
2020-03-20 14:52:53 -0300

    Resolução do exercício proposto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("|  Bárbara  |");
+            Console.WriteLine("+-----------+");
+        }
+    }
+}
